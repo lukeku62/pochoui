65b48f1ce1eb6f09cb1aac8c6182d7fddc72cc61
lukeku62 (Luca Reverberi) <luca.reverberi62@gmail.com>
2024-11-22 16:52:53 +0100

    fix: min

--- packages/date-picker/src/index.ts
+++ packages/date-picker/src/index.ts
@@ -1,2 +1,7 @@
-export { DatePicker } from './lib/DatePicker';
-export type { DatePickerProps } from './lib/DatePicker';
+import { DatePicker as DatePickerComponent, DatePickerProps as Props } from './lib/DatePicker';
+
+// Re-export with all types inline
+export type DatePickerProps = Props;
+export const DatePicker: React.FC<DatePickerProps> = DatePickerComponent;
+
+export default DatePicker;
