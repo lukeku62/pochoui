a657483b456211afa601fe4a1f5b637acc364d89
Luca Reverberi <81616027+lukeku62@users.noreply.github.com>
2024-11-22 09:52:21 +0100

    chore: update package.json (#5)

--- packages/date-picker/src/lib/DatePicker.tsx
+++ packages/date-picker/src/lib/DatePicker.tsx
@@ -1,9 +1,28 @@
+import { useRef } from "react";
 
-export function DatePicker() {
+export const DatePicker = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const toggleDialog = () => {
+    if (dialogRef.current?.open) {
+      dialogRef.current.close();
+    } else {
+      dialogRef.current?.show();
+    }
+  };
+
   return (
-    <div className={"container"}>
-      <h1>Welcome to DatePicker!</h1>
-    </div>
+    <>
+      <button onClick={toggleDialog}>Toggle Dialog</button>
+      <dialog
+        ref={dialogRef}
+        className="m-0 p-4 rounded-lg shadow-lg border border-gray-200 absolute backdrop:hidden"
+      >
+        <form method="dialog" className="container">
+          <button>Close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
 
